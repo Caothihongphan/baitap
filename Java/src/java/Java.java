d53b0118a814d2406d53d9df6cef7ce4edbb8026
Caothihongphan <63287680+Caothihongphan@users.noreply.github.com>
2020-04-10 23:12:16 -0700

    Baitapjava

--- Java/src/java/Java.java
+++ Java/src/java/Java.java
@@ -5,16 +5,48 @@
  */
 package java;
 
-/**
- *
- * @author DELL
- */
+
+
+ 
 public class Java {
 
+    String name;
+    int tuoi;
+    String diachi;
+    Double tienluong;
+    int tongsohl;
+    
+    Java(){System.out.println(name+" "+tuoi+""+diachi+""+tienluong+""+tongsohl);}
+    Java(String n , int t , String dc, Double tl, int tsh ){  
+    
+    name = n;  
+    tuoi = t;
+    diachi = dc;
+    tienluong = tl;
+    tongsohl = tsh;
+    }  
+  public void setName(String name) { this.name = name; }
+  public void setTuoi(int tuoi) { this.tuoi= tuoi; }
+  public void setDiachi() { this.diachi = diachi; }
+  public double getTienluong() { return this.tienluong; }
+  public int getTongsohl(){return this.tongsohl;}
+  
+    
+  public String getHienthitt() {
+    String thongTin = "";
+    thongTin = "ho ten: " + this.name + ", tuoi: " + this.tuoi + ", dia chi: " + this.diachi
+      + ", tong luong: " + this.tienluong + ", tong so gio lam: " + this.tongsohl;
+
+    System.out.println(thongTin);
+    return thongTin;
+   }
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+         Java bai1 = new Java();
+         
+         
         // TODO code application logic here
     }
     
